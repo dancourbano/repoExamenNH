9e0e7da845f29b86db478b231ed54c39979fe4af
root <danielcam.inf@gmail.com>
2020-06-05 00:44:25 -0500

    avance pantalla

---
+++ currency-service/src/main/java/com/jaed/currencyservice/WebGuiController.java
@@ -0,0 +1,22 @@
+package com.jaed.currencyservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/gui")
+public class WebGuiController {
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
+	
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String raiz(Model model) {
+		model.addAttribute("name", ""); 
+		return "conversion";
+	}
+}
