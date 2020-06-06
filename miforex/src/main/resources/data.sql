33b8957b5452d791b22e4e85fdd69058c419f8da
svalderr <svalderr@TRJ-22PP4L2.usersad.everis.int>
2020-06-05 21:04:58 -0500

    datos de 16 combinaciones para 4 tipos de monedas

--- miforex/src/main/resources/data.sql
+++ miforex/src/main/resources/data.sql
@@ -1,6 +1,35 @@
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10001,'USD','PEN',3.28,0);
+values(10001,'USD','PEN',3.38,0);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10002,'EUR','PEN',3.80,0);
+values(10002,'EUR','PEN',3.82,0);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
 values(10003,'YUA','PEN',0.48,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10004,'PEN','PEN',1.00,0);
+
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10005,'USD','USD',1.00,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10006,'EUR','USD',1.13,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10007,'YUA','USD',0.14,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10008,'PEN','USD',0.29,0);
+
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10009,'USD','EUR',0.88,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10010,'EUR','EUR',1.00,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10011,'YUA','EUR',0.12,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10012,'PEN','EUR',0.26,0);
+
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10013,'USD','YUA',7.09,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10014,'EUR','YUA',8.02,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10015,'YUA','YUA'1.00,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10016,'PEN','YUA',2.05,0);
