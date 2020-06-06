f4c1fb19f260ffc951432afa45bbc03aa78359fb
cdavilaf <cdavilaf@TRJ-BCZ5RV2.usersad.everis.int>
2020-06-05 23:19:56 -0500

    integracion front con back

--- currency-service/src/main/java/com/jaed/currencyservice/CurrencyConversionController.java
+++ currency-service/src/main/java/com/jaed/currencyservice/CurrencyConversionController.java
@@ -8,8 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,7 +22,7 @@ public class CurrencyConversionController {
 	  private Logger logger = LoggerFactory.getLogger(this.getClass());
 	  
 	  @Autowired
-	  private CurrencyExchangeServiceProxy proxy;
+	  private CurrencyExchangeServiceProxy proxy;	  
 	  
 	  @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	  public CurrencyConversionBean convertCurrency(@PathVariable String from, 
